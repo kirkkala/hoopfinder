db1c0233e6225762bb8d2b8ca2dc9329491dbeb1
Timo Kirkkala <timo.kirkkala@digitalistgroup.com>
2026-09-12 13:35:36 +0300

    Maplibre 6 compatibility

--- src/components/court/CourtMiniMap.tsx
+++ src/components/court/CourtMiniMap.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import Map, { Marker } from "react-map-gl/maplibre";
+import { setWorkerUrl } from "maplibre-gl";
 import type { Court } from "@/lib/courts";
 import { MAP_STYLE } from "@/lib/constants";
+
+setWorkerUrl("/maplibre/maplibre-gl-worker.mjs");
 
 export function CourtMiniMap({ court }: { court: Court }) {
   return (
